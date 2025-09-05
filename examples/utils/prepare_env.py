6c1254cada4e41313844569d4f2dea925d5a9b82
Elliot <agony74@qq.com>
2025-09-05 11:42:02 +0800

    暂时不过滤FAILED

--- examples/utils/prepare_env.py
+++ examples/utils/prepare_env.py
@@ -8,7 +8,7 @@ from configparser import ConfigParser
 def get_api_key():
     config = ConfigParser()
     config_file_path = os.path.join(
-        pathlib.Path(__file__).parent.resolve(), "..", "config.ini.example"
+        pathlib.Path(__file__).parent.resolve(), "..", "config.ini"
     )
     config.read(config_file_path)
     return config["keys"]["api_key"]
